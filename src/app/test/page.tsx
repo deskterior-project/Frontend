60b1121d20b3b644236d21727c0db45029b7fe04
Yeon <lyle.usn@gmail.com>
2025-11-24 22:59:28 +0900

    feat: version 제거, label&helperText optional, 샘플 페이지 수정

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,25 +6,17 @@ import { useState } from "react";
 export default function TestPage() {
   const [name, setName] = useState("");
   return (
-    <div className="flex flex-col ml-52 mt-8 gap-8">
-      <TextField
-        id="1"
-        state="default"
-        version="pc"
-        value={name}
-        onChange={setName}
-      />
-      {/* <TextField id="2" state="focused" version="pc" />
-      <TextField id="3" state="success" version="pc" />
-      <TextField id="4" state="error" version="pc" />
-      <TextField id="5" state="disabled" version="pc" />
-      <TextField id="6" state="complete" version="pc" />
-      <TextField id="7" state="default" version="mo" />
-      <TextField id="8" state="focused" version="mo" />
-      <TextField id="9" state="success" version="mo" />
-      <TextField id="10" state="error" version="mo" />
-      <TextField id="11" state="disabled" version="mo" />
-      <TextField id="12" state="complete" version="mo" /> */}
+    <div className="flex flex-col ml-52 mt-8 gap-8 w-[335px]">
+      <TextField id="1" state="default" value={name} />
+      <TextField id="3" state="success" value={name} />
+      <TextField id="4" state="error" value={name} />
+      <TextField id="5" state="disabled" value={name} />
+      <TextField id="6" state="complete" value={name} />
+      <TextField id="7" state="default" value={name} />
+      <TextField id="9" state="success" value={name} />
+      <TextField id="10" state="error" value={name} />
+      <TextField id="11" state="disabled" value={name} />
+      <TextField id="12" state="complete" value={name} />
     </div>
   );
 }
--- src/components/ui/TextField.tsx
+++ src/components/ui/TextField.tsx
@@ -1,24 +1,20 @@
 "use client";
 
 import React, { useState } from "react";
-import clsx from "clsx";
 import { default as DismissCircle } from "@/assets/dismiss-circle-regular.svg";
 import { cva } from "class-variance-authority";
 
-type TextFieldProps = {
+interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label?: string;
   helperText?: string;
   value: string;
-  onChange: (value: string) => void;
   state?: "default" | "success" | "error" | "disabled" | "complete";
   placeholder?: string;
-  version: "pc" | "mo";
-};
+}
 
-const labelStyle = cva("", {
+const labelVariants = cva("pc:typo-pc-title-xs700 mo:typo-mo-title-s700", {
   variants: {
-    version: { pc: "typo-pc-title-xs700", mo: "typo-mo-title-s700" },
     state: {
       default: "text-black-900",
       focused: "text-black-900",
@@ -29,20 +25,19 @@ const labelStyle = cva("", {
     },
   },
   defaultVariants: {
-    version: "pc",
     state: "default",
   },
 });
 
-const inputWrapperStyle = cva("border flex items-center py-2 px-3", {
+const inputWrapperVariants = cva("inset-ring-1 flex items-center py-2 px-3", {
   variants: {
     state: {
-      default: "border-black-600 bg-white-200",
-      focused: "border-black-900 bg-white-200",
-      success: "border-success-700 bg-white-200",
-      error: "border-error-600 bg-white-200",
-      disabled: "border-black-500 bg-white-400",
-      complete: "border-black-600 bg-white-200",
+      default: "inset-ring-black-600 bg-white-200",
+      focused: "inset-ring-black-900 bg-white-200",
+      success: "inset-ring-success-700 bg-white-200",
+      error: "inset-ring-error-600 bg-white-200",
+      disabled: "inset-ring-black-500 bg-white-400",
+      complete: "inset-ring-black-600 bg-white-200",
     },
   },
   defaultVariants: {
@@ -50,27 +45,27 @@ const inputWrapperStyle = cva("border flex items-center py-2 px-3", {
   },
 });
 
-const inputStyle = cva("w-full outline-none", {
-  variants: {
-    version: { pc: "typo-pc-body-m400", mo: "typo-mo-body-m400" },
-    state: {
-      default: "text-black-600",
-      focused: "text-black-900",
-      success: "text-black-900",
-      error: "text-black-900",
-      disabled: "text-black-500",
-      complete: "text-black-900",
+const inputVariants = cva(
+  "w-full outline-none pc:typo-pc-body-m400 mo:typo-mo-body-m400",
+  {
+    variants: {
+      state: {
+        default: "text-black-600",
+        focused: "text-black-900",
+        success: "text-black-900",
+        error: "text-black-900",
+        disabled: "text-black-500",
+        complete: "text-black-900",
+      },
     },
-  },
-  defaultVariants: {
-    version: "pc",
-    state: "default",
-  },
-});
+    defaultVariants: {
+      state: "default",
+    },
+  }
+);
 
-const dismissStyle = cva("cursor-pointer", {
+const dismissCircleVariants = cva("cursor-pointer pc:size-6 mo:size-5", {
   variants: {
-    version: { pc: "w-6 h-6", mo: "w-5 h-5" },
     state: {
       default: "text-black-600",
       focused: "text-black-900",
@@ -80,63 +75,67 @@ const dismissStyle = cva("cursor-pointer", {
       complete: "text-black-600",
     },
   },
-  defaultVariants: { version: "pc", state: "default" },
+  defaultVariants: { state: "default" },
 });
+
+const helperTextVariants = cva(
+  "pc:typo-pc-body-m400 mo:typo-mo-body-m400 text-black-600",
+  {
+    variants: {
+      state: {
+        default: "",
+        focused: "",
+        success: "",
+        error: "",
+        disabled: "text-black-500",
+        complete: "",
+      },
+    },
+    defaultVariants: {
+      state: "default",
+    },
+  }
+);
 
 export default function TextField({
   id,
   label = "label",
   helperText = "helper text",
-  value,
-  onChange,
   placeholder = "input text",
-  version,
   state = "default",
 }: TextFieldProps) {
-  const isPc = version === "pc";
   const isDisabled = state === "disabled";
   const [focused, setFocused] = useState(false);
   const finalState = isDisabled ? "disabled" : focused ? "focused" : state;
 
-  const handleClear = () => {
-    onChange("");
-  };
-
   return (
-    <div className="flex flex-col gap-2 w-[335px]">
-      <label
-        htmlFor={id}
-        className={labelStyle({ state: finalState, version })}
-      >
-        {label}
-      </label>
+    <div className="flex flex-col gap-2 w-full">
+      {label && (
+        <label htmlFor={id} className={labelVariants({ state: finalState })}>
+          {label}
+        </label>
+      )}
 
-      <div className={inputWrapperStyle({ state: finalState })}>
+      <div className={inputWrapperVariants({ state: finalState })}>
         <input
           id={id}
           type="text"
-          value={value}
           onFocus={() => setFocused(true)}
           onBlur={() => setFocused(false)}
           disabled={isDisabled}
-          onChange={(e) => onChange(e.target.value)}
           placeholder={placeholder}
-          className={inputStyle({ state: finalState, version })}
+          className={inputVariants({ state: finalState })}
         />
         <DismissCircle
-          onClick={() => handleClear()}
-          className={dismissStyle({ version, state: finalState })}
+          className={dismissCircleVariants({ state: finalState })}
         />
       </div>
 
-      <span
-        className={clsx(
-          state === "disabled" ? "text-black-500" : "text-black-600",
-          isPc ? "typo-pc-body-m400" : "typo-mo-body-m400"
-        )}
-      >
-        {helperText}
-      </span>
+      {helperText && (
+        <span className={helperTextVariants({ state: finalState })}>
+          {helperText}
+        </span>
+      )}
     </div>
   );
 }
