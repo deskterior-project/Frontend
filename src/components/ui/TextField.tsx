07848aadf05574699e3f9f5b94dda09db3a29630
Yeon <lyle.usn@gmail.com>
2025-11-24 21:36:14 +0900

    feat: cva 적용

--- src/components/ui/TextField.tsx
+++ src/components/ui/TextField.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import clsx from "clsx";
 import { default as DismissCircle } from "@/assets/dismiss-circle-regular.svg";
+import { cva } from "class-variance-authority";
 
 type TextFieldProps = {
   id: string;
@@ -15,44 +16,72 @@ type TextFieldProps = {
   version: "pc" | "mo";
 };
 
-const STYLE_BY_STATE = {
-  default: {
-    text: "text-black-900",
-    border: "border-black-600",
-    input: "text-black-600",
-    dismiss: "text-black-600",
+const labelStyle = cva("", {
+  variants: {
+    version: { pc: "typo-pc-title-xs700", mo: "typo-mo-title-s700" },
+    state: {
+      default: "text-black-900",
+      focused: "text-black-900",
+      success: "text-success-700",
+      error: "text-error-600",
+      disabled: "text-black-400",
+      complete: "text-black-900",
+    },
   },
-  focused: {
-    text: "text-black-900",
-    border: "border-black-900",
-    input: "text-black-900",
-    dismiss: "text-black-900",
+  defaultVariants: {
+    version: "pc",
+    state: "default",
   },
-  success: {
-    text: "text-success-700",
-    border: "border-success-700",
-    input: "text-black-900",
-    dismiss: "text-black-900",
+});
+
+const inputWrapperStyle = cva("border flex items-center py-2 px-3", {
+  variants: {
+    state: {
+      default: "border-black-600 bg-white-200",
+      focused: "border-black-900 bg-white-200",
+      success: "border-success-700 bg-white-200",
+      error: "border-error-600 bg-white-200",
+      disabled: "border-black-500 bg-white-400",
+      complete: "border-black-600 bg-white-200",
+    },
   },
-  error: {
-    text: "text-error-600",
-    border: "border-error-600",
-    input: "text-black-900",
-    dismiss: "text-black-900",
+  defaultVariants: {
+    state: "default",
   },
-  disabled: {
-    text: "text-black-400",
-    border: "border-black-500",
-    input: "text-black-500",
-    dismiss: "text-black-500",
+});
+
+const inputStyle = cva("w-full outline-none", {
+  variants: {
+    version: { pc: "typo-pc-body-m400", mo: "typo-mo-body-m400" },
+    state: {
+      default: "text-black-600",
+      focused: "text-black-900",
+      success: "text-black-900",
+      error: "text-black-900",
+      disabled: "text-black-500",
+      complete: "text-black-900",
+    },
   },
-  complete: {
-    text: "text-black-900",
-    border: "border-black-600",
-    input: "text-black-900",
-    dismiss: "text-black-600",
+  defaultVariants: {
+    version: "pc",
+    state: "default",
   },
-} as const;
+});
+
+const dismissStyle = cva("cursor-pointer", {
+  variants: {
+    version: { pc: "w-6 h-6", mo: "w-5 h-5" },
+    state: {
+      default: "text-black-600",
+      focused: "text-black-900",
+      success: "text-black-900",
+      error: "text-black-900",
+      disabled: "text-black-500",
+      complete: "text-black-600",
+    },
+  },
+  defaultVariants: { version: "pc", state: "default" },
+});
 
 export default function TextField({
   id,
@@ -68,7 +97,6 @@ export default function TextField({
   const isDisabled = state === "disabled";
   const [focused, setFocused] = useState(false);
   const finalState = isDisabled ? "disabled" : focused ? "focused" : state;
-  const { text, border, input, dismiss } = STYLE_BY_STATE[finalState];
 
   const handleClear = () => {
     onChange("");
@@ -78,21 +106,12 @@ export default function TextField({
     <div className="flex flex-col gap-2 w-[335px]">
       <label
         htmlFor={id}
-        className={clsx(
-          text,
-          isPc ? "typo-pc-title-xs700" : "typo-mo-title-s700"
-        )}
+        className={labelStyle({ state: finalState, version })}
       >
         {label}
       </label>
 
-      <div
-        className={clsx(
-          "border flex items-center py-2 px-3",
-          state === "disabled" ? "bg-white-400" : "bg-white-200",
-          border
-        )}
-      >
+      <div className={inputWrapperStyle({ state: finalState })}>
         <input
           id={id}
           type="text"
@@ -102,19 +121,11 @@ export default function TextField({
           disabled={isDisabled}
           onChange={(e) => onChange(e.target.value)}
           placeholder={placeholder}
-          className={clsx(
-            "w-full outline-none",
-            input,
-            isPc ? "typo-pc-body-m400" : "typo-mo-body-m400"
-          )}
+          className={inputStyle({ state: finalState, version })}
         />
         <DismissCircle
           onClick={() => handleClear()}
-          className={clsx(
-            isPc ? "w-6 h-6" : "w-5 h-5",
-            "cursor-pointer",
-            dismiss
-          )}
+          className={dismissStyle({ version, state: finalState })}
         />
       </div>
 
