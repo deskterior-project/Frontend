ecd4b038cf88868bc581798ea93cd3a5abf6a93e
정민석 <minskyson7@gmail.com>
2026-02-04 14:34:25 +0900

    chore: SUIT 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
+import { suit } from "@/assets/fonts/fonts";
 import AlertModal from "@/components/ui/Modal/AlertModal/AlertModal";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "De:Foco_",
@@ -25,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={suit.className}>
         {children}
         <AlertModal />
       </body>
---
+++ src/assets/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from "next/font/local";
+
+export const suit = localFont({
+  src: "./SUIT-Variable.woff2",
+  variable: "--font-suit",
+  weight: "100 900",
+  display: "swap",
+});
