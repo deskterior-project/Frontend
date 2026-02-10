38119f58bfe51d061f9a38c988870b1c16af6ee5
정민석 <minskyson7@gmail.com>
2026-02-10 09:23:59 +0900

    feat: TabMenu active 상태로 변경하도록 수정

--- src/components/ui/Button/TabMenu.tsx
+++ src/components/ui/Button/TabMenu.tsx
@@ -2,9 +2,10 @@ import { cn } from "@/hooks/cn";
 
 interface TabMenuProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  active: boolean;
 }
 
-const TabMenu = ({ children, className, ...props }: TabMenuProps) => {
+const TabMenu = ({ children, active, className, ...props }: TabMenuProps) => {
   return (
     <button
       {...props}
@@ -12,11 +13,12 @@ const TabMenu = ({ children, className, ...props }: TabMenuProps) => {
         "flex items-center justify-center gap-1 cursor-pointer",
         "px-2 py-1 pc:px-4 pc:py-2",
         "typo-mo-body-s500 pc:typo-pc-body-s500",
-        "bg-white-200 hover:bg-white-500 active:bg-white-500 disabled:bg-white-500",
-        "text-black-900 hover:text-black-800 active:text-black-800 disabled:text-black-400",
+        "bg-white-200 hover:bg-white-500 disabled:bg-white-500",
+        "text-black-900 hover:text-black-800 disabled:text-black-400",
         "disabled:cursor-not-allowed",
-        "inset-ring-1 inset-ring-black-900 hover:inset-ring-black-800 active:inset-ring-black-800 disabled:inset-ring-black-400",
-        className
+        "inset-ring-1 inset-ring-black-900 hover:inset-ring-black-800 disabled:inset-ring-black-400",
+        active && "bg-white-500 text-black-800 inset-ring-black-800",
+        className,
       )}
     >
       {children}
