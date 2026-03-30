e70d0944d040225d097de200987e8c230ee3ea99
정민석 <minskyson7@gmail.com>
2026-03-30 09:30:18 +0900

    ui: 팔로우 버튼 컴포넌트 추가 및 프로필 섹션 업데이트

--- src/app/(user)/[userId]/page.tsx
+++ src/app/(user)/[userId]/page.tsx
@@ -3,14 +3,41 @@ import Profile from "@/components/section/my-page/Profile";
 import BasicButton from "@/components/ui/Button/BasicButton";
 import HeaderMo from "@/components/ui/Header/HeaderMo";
 import { useParams } from "next/navigation";
-import React from "react";
-import Follow from "@/assets/person-add-regular.svg";
+import React, { useState } from "react";
+import AddFollow from "@/assets/person-add-regular.svg";
+import AvailableFollow from "@/assets/person-available-regular.svg";
 
 const userInfo = {
     profileImageUrl: "/image.png",
     nickname: "nickname",
     following: 999,
     followers: 999,
+};
+
+const FollowButton = () => {
+    const [isFollowing, setIsFollowing] = useState(false);
+    const handleFollowButtonClick = () => {
+        setIsFollowing(!isFollowing);
+    };
+    return isFollowing ? (
+        <BasicButton
+            variant="tertiary"
+            size="small"
+            onClick={handleFollowButtonClick}
+        >
+            팔로잉
+            <AvailableFollow className="pc:size-5 size-4" />
+        </BasicButton>
+    ) : (
+        <BasicButton
+            variant="secondary"
+            size="small"
+            onClick={handleFollowButtonClick}
+        >
+            팔로우
+            <AddFollow className="pc:size-5 size-4" />
+        </BasicButton>
+    );
 };
 
 const UserPage = () => {
@@ -22,10 +49,7 @@ const UserPage = () => {
             <section className="pc:max-w-300 inset-ring-black-900 pc:py-8 pc:px-8 pc:flex-row pc:justify-between pc:items-center flex w-full flex-col gap-4 px-4 py-5 inset-ring-1">
                 <Profile isMe={false} userInfo={userInfo} />
                 <div className="pc:pl-0 pl-15">
-                    <BasicButton variant="secondary" size="small">
-                        팔로우
-                        <Follow className="pc:size-5 size-4" />
-                    </BasicButton>
+                    <FollowButton />
                 </div>
             </section>
             <section className="pc:max-w-300 pc:gap-6 flex w-full flex-col gap-4">
